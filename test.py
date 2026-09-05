709e76c6ef9eebe501069122001035ef3b57e688
Mrinmoy_1315 <mrinmoymatilal1315@gmail.com>
2026-09-05 21:08:56 +0530

    All final changes Flight and Tavily Search added

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 from tools.tavily_tool import tavily_search
+from tools.flight_tool import search_flights
 
-res = tavily_search("Best Hotels in India")
+# res = tavily_search("Best Hotels in India")
+# print(res)
+
+res= search_flights("Plan a 7 days Switzerland trip from India")
 print(res)
